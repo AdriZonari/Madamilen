f9344731ee26b1b022145e4542cbed498da52d5c
AdriZonari <36879308+AdriZonari@users.noreply.github.com>
2018-04-17 14:17:57 +0200

    uppdaterad

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import Bars from 'react-icons/lib/fa/bars'
-//import ImageResponsive, {Source} from 'react-image-responsive';
-//import PropTypes from 'prop-types';
-
-
+import logotyp_png from './logotyp_png.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        {/*<div className="Madamilen-logo">
-        <ImageResponsive type="image" src="https://madamilen.files.wordpress.com/2017/06/logotyp_png.png?w=380" width="380" height="68"> 
-        <Source src="https://madamilen.files.wordpress.com/2017/06/logotyp_png.png?w=380" maxWidth={380} /> 
-        <Source src="https://madamilen.files.wordpress.com/2017/06/logotyp_png.png?w=760" maxWidth={760} />
-        <Source src="https://madamilen.files.wordpress.com/2017/06/logotyp_png.png?w=150" maxWidth={150} />
-        <Source src="https://madamilen.files.wordpress.com/2017/06/logotyp_png.png?w=300" maxWidth={300} />
-    </ImageResponsive>
-        </div>*/}
-          <img src="https://madamilen.files.wordpress.com/2017/06/logotyp_png.png?w=380" width="380" height="68" className="Madamilen-logo" alt="logo" />
-          <div className="menu">
-          <Bars/> Meny
-          </div>
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h1 className="App-title">Kulinarisk matvandring i Skåne</h1>
+          <img src={logotyp_png} width="358" height="64" className="Madamilen-logo" alt="logo" />
         </header>
-        <p className="App-intro">
-          Madamilen är en matvandring i olika delar av Skåne där man går eller cyckla till 10 olika restaurang och får provsmaka deras stjärnrätt
-        </p>
-          <h2>Madamilen Landskrona 12/06</h2>
-      
-      </div>
+    
+    <div className="App-intro">
+        <div className="pink">
+        </div>
+        <div className="foreground">
+        <h1>Välkommen!</h1>
+          <p>Madamilen är en kulinarisk
+           vandring under dagtid. Ett möte mellan matglada.
+            Tio stopp och tio smaksensationer.</p>
+        <button className="stad">Landskrona</button>
+        <button className="kommande">På Gång</button>
+        </div>
+        
+     </div>
+</div>
     );
   }
 }
