51f43da8ea7bed9ebfdc251538b9fc6e35692331
AdriZonari <36879308+AdriZonari@users.noreply.github.com>
2018-04-23 11:56:07 +0200

    uppdaterad med map.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import logotyp_png from './logotyp_png.png'
 import prickar_png from './prickar_png.png'
+import { MapContainer } from './map.js';
 
 class App extends Component {
   render() {
@@ -16,7 +17,9 @@ class App extends Component {
           <p>Madamilen är en kulinarisk
            vandring under dagtid. Ett möte mellan matglada.
             Tio stopp och tio smaksensationer.</p>
-        <button className="stad">Landskrona</button>
+        <button className="stad">Landskrona
+          <link><MapContainer/></link>
+        </button>
         <img className="prickar" src={prickar_png} width="345" height="30" alt="prickar" />
         <button className="kommande">Kommande Madamilen</button>
         </div>
