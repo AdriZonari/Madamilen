936e1fc3c34291ce33a3f422810e0177d9fcfdf7
AdriZonari <36879308+AdriZonari@users.noreply.github.com>
2018-04-24 13:34:15 +0200

    Add files via upload

--- src/map.js
+++ src/map.js
@@ -1,26 +1,58 @@
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, GoogleApiWrapper} from 'google-maps-react';
 import React, { Component } from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
 
 
 export class MapContainer extends Component {
+
+    loadMap() {
+        if (this.props && this.props.google) {
+          // google is available
+          const {google} = this.props;
+          const maps = google.maps;
+    
+          const mapRef = this.refs.map;
+          const node = ReactDOM.findDOMNode(mapRef);
+
+        let zoom = 14;
+        let lat = 37.774929;
+        let lng = -122.419416;
+        const center = new maps.LatLng(lat, lng);
+        const mapConfig = Object.assign({}, {
+        center: center,
+        zoom: zoom
+      })
+      this.map = new maps.Map(node, mapConfig);
+        }
+    }
+
+    componentDidMount() {
+      this.loadMap();
+    }
+  
+    componentDidUpdate(prevProps, prevState) {
+      if (prevProps.google !== this.props.google) {
+        this.loadMap();
+      }
+    }
+
+
   render() {
+    
+    const style = { width : '100vw',
+                  height: '100vh'
+    }
     return (
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
-    );
-  }
+       <div ref='map'>Laddar kartan...</div>,
+        <div >
+        <Map google={this.props.google}
+              style={style} />
+        </div>    
+        )
+    }
 }
  
 export default GoogleApiWrapper({
-  apiKey : (AIzaSyAk9nBuXiuBcbnY9VP8bFWrJs30MM0eINM)
+  apiKey : "AIzaSyAk9nBuXiuBcbnY9VP8bFWrJs30MM0eINM"
 })(MapContainer)
