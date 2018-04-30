0cf3799b0ef8b459ef85b76f164e66292944d8f2
Hogerkrok <36738554+Hogerkrok@users.noreply.github.com>
2018-04-30 18:40:27 +0200

    Google Maps React med fungerande markör

--- src/map.js
+++ src/map.js
@@ -1,53 +1,96 @@
+import React from 'react';
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
+/*
+Jag har inga mappar 
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import { typography } from 'material-ui/styles';*/
 
-import React, { Component } from "react";
-import GoogleMapReact from "google-map-react";
-import if_Marker_66990 from "./if_Marker_66990.png";
-
-import './App.css';
-import ReactDOM from 'react-dom';
 
 
-export const AnyReactComponent = ({ text, img }) => (
-  <div>
-    {text},{img}
-  </div>
-);
+class GoogleMapsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    }
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+  }
 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 55.866667,
-      lng: 12.833333
-    },
-    zoom: 11,
-    text: "Landskrona",
-    language: "sv",
-    region: "sv"
-  };
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
 
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
 
   render() {
-    
-    return (
+    const style = {
+      width: '50vw',
+      height: '75vh',
+      'marginLeft': 'auto',
+      'marginRight': 'auto'
+    }
 
-      // Important! Always set the container height explicitly
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAk9nBuXiuBcbnY9VP8bFWrJs30MM0eINM" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={55.90456}
-            lng={12.8088}
-            text={"Erikstorps Kungsgård"}
-          >
-            <img src={if_Marker_66990} alt="" />
-          </AnyReactComponent>
-        </GoogleMapReact>
-      </div>
+    return (
+      <Map
+        item
+        xs = { 12 }
+        style = { style }
+        google = { this.props.google }
+        onClick = { this.onMapClick }
+        zoom = { 18 }
+        initialCenter = {{ lat: 55.904582, lng: 12.808880 }}
+      >
+        <Marker
+          onClick = { this.onMarkerClick }
+          title = { 'Changing Colors Garage' }
+          position = {{ lat: 55.904582, lng: 12.808880 }}
+          name = { 'Changing Colors Garage' }
+        />
+      
+        
+      </Map>
     );
   }
 }
 
-export default SimpleMap;
+export default GoogleApiWrapper({
+    api: (process.env.AIzaSyAk9nBuXiuBcbnY9VP8bFWrJs30MM0eINM)
+})(GoogleMapsContainer)
+
+/*
+Detta ska egentligen vara efter MARKER för att få en textruta m.m och för få det som är impoterat högst upp i koden att fungera
+<InfoWindow
+          marker = { this.state.activeMarker }
+          visible = { this.state.showingInfoWindow }
+        >
+          <Paper>
+            <Typography
+              variant = 'headline'
+              component = 'h4'
+            >
+              Changing Colors Garage
+            </Typography>
+            <Typography
+              component = 'p'
+            >
+              98G Albe Dr Newark, DE 19702 <br />
+              302-293-8627
+            </Typography>
+          </Paper>
+        </InfoWindow>*/
