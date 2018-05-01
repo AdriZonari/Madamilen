e818a254049fb0b372b868ca4fed073dd5214b92
AdriZonari <36879308+AdriZonari@users.noreply.github.com>
2018-05-01 23:31:31 +0200

    uppdated map med markers och info window
    Info window dock dyker upp för både när man clickar på Grand Hotel och korrekt när man clickar på Malmstens. Jag upptäckte ett problem i "bound()" när jag kollar på console, men kan inte riktig identifiera problemet och hur jag löser det. Hjälp, tack!

--- src/map.js
+++ src/map.js
@@ -1,53 +1,105 @@
-
-import React, { Component } from "react";
-import GoogleMapReact from "google-map-react";
-import if_Marker_66990 from "./if_Marker_66990.png";
-
-import './App.css';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
+import Paper from 'material-ui/Paper';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 
-export const AnyReactComponent = ({ text, img }) => (
-  <div>
-    {text},{img}
-  </div>
-);
-
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 55.866667,
-      lng: 12.833333
-    },
-    zoom: 11,
-    text: "Landskrona",
-    language: "sv",
-    region: "sv"
-  };
-
+class GoogleMapsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    }
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+  }
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
 
   render() {
-    
+    const style = {
+      width: '100',
+      height: '100',
+      'marginLeft': 'auto',
+      'marginRight': 'auto'
+    }
     return (
-
-      // Important! Always set the container height explicitly
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAk9nBuXiuBcbnY9VP8bFWrJs30MM0eINM" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+      <Map
+        item
+        xs = { 12 }
+        style = { style }
+        google = { this.props.google }
+        onClick = { this.onMapClick }
+        zoom = { 14 }
+        initialCenter = {{ lat: 55.7047, lng: 13.1910 }}
+      >
+        <Marker
+          onClick = { this.onMarkerClick }
+          title = { 'Grand Hotel' }
+          position = {{ lat: 55.7039, lng: 13.1890 }}
+          name = { 'Grand Hotel' }
+        />
+        <InfoWindow
+          marker = { this.state.activeMarker }
+          visible = { this.state.showingInfoWindow }
         >
-          <AnyReactComponent
-            lat={55.90456}
-            lng={12.8088}
-            text={"Erikstorps Kungsgård"}
-          >
-            <img src={if_Marker_66990} alt="" />
-          </AnyReactComponent>
-        </GoogleMapReact>
-      </div>
+        <MuiThemeProvider>
+          <Paper>
+          <h4 variant = 'headline'>
+              Grand Hotel
+            </h4>
+            <p>
+              Bantorget 1, 222 29 <br />
+              046-280 61 00
+            </p>
+          </Paper>
+          </MuiThemeProvider>
+        </InfoWindow>
+
+        <Marker
+          onClick = { this.onMarkerClick }
+          title = { 'Malmstens Fisk & Kök' }
+          position = {{ lat: 55.701538, lng: 13.193845 }}
+          name = { 'Malmstens Fisk & Kök' }
+        />
+        <InfoWindow
+          marker = { this.state.activeMarker }
+          visible = { this.state.showingInfoWindow }
+        >
+        <MuiThemeProvider>
+          <Paper>
+            <h4 variant = 'headline'>
+              Malmstens Fisk och Kök
+            </h4>
+            <p>
+              Västra Mårtensgatan 9, 223 51 <br />
+              046-12 63 54
+            </p>
+          </Paper>
+          </MuiThemeProvider>
+        </InfoWindow>
+      </Map>
     );
   }
 }
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyChp6lpBy8QtrTlHuIzPoRSTY7eoTZVRuA'
+})(GoogleMapsContainer)
 
-export default SimpleMap;
+  
