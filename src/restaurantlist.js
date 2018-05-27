66bdfd9fa5f49f57a41856256f0965d4894aa243
AdriZonari <36879308+AdriZonari@users.noreply.github.com>
2018-05-28 00:16:52 +0200

    Add files via upload

--- src/restaurantlist.js
+++ src/restaurantlist.js
@@ -11,15 +11,15 @@ export class RestaurantList extends React.Component {
   
   <div className="RestaurantList">
         <ul>
-        <li><Link to = "/foteviken">Fotevikens Restaurang och Café</Link><LikeButton /></li>
-        <li><Link to = "/icatoppen">Delitorget på ICA Toppen</Link><LikeButton /></li>
-        <li><Link to = "/cafeidetgrona">Café i det Gröna</Link><LikeButton /></li>
-        <li><Link to = "/vespa">Vespa</Link><LikeButton /></li>
-        <li><Link to = "/kottochgrill">Höllviken Kött och Grill</Link><LikeButton /></li>
-        <li><Link to = "/nasetbythesea">Näset by the Sea</Link><LikeButton /></li>
-        <li><Link to = "/shakespearepub">Shakespeare Pub</Link><LikeButton /></li>
-        <li><Link to = "/gateau">Gateau</Link><LikeButton /></li>
-        <li><Link to = "/smakverkeri">Smakverkeri</Link><LikeButton /></li>
+        <li><Link to = "/foteviken">Fotevikens Restaurang och Café</Link> <LikeButton /></li>
+        <li><Link to = "/icatoppen">Delitorget på ICA Toppen</Link> <LikeButton /></li>
+        <li><Link to = "/cafeidetgrona">Café i det Gröna</Link> <LikeButton /></li>
+        <li><Link to = "/vespa">Vespa</Link> <LikeButton /></li>
+        <li><Link to = "/kottochgrill">Höllviken Kött och Grill</Link> <LikeButton /></li>
+        <li><Link to = "/nasetbythesea">Näset by the Sea</Link> <LikeButton /></li>
+        <li><Link to = "/shakespearepub">Shakespeare Pub</Link> <LikeButton /></li>
+        <li><Link to = "/gateau">Gateau</Link> <LikeButton /></li>
+        <li><Link to = "/smakverkeri">Smakverkeri</Link> <LikeButton /></li>
 </ul>
     </div>
     )
